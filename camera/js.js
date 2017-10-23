30d1c8e24603f5f6b74ed76bed870420a38c0fb4
janwork <janwork@163.com>
2017-10-23 23:29:06 +0800

    准备工作

---
+++ camera/js.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jan on 2017/10/23.
+ */
+
+
+window.onload = function () {
+    const canvas = document.querySelector('canvas');
+    const video  = document.querySelector('video');
+    const ctx = canvas.getContext('2d');
+    const img = document.querySelector('#myimg');
+    const slider = document.querySelector('.rgb');
+    const a = document.querySelector('a');
+    
+
+}
+
+
